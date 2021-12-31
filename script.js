888f56a02cf2eed435937219b740470c1397ea7b
Berenice Sanchez <sanchezberenice09@gmail.com>
2021-12-30 23:17:47 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let saludo = "Buen dia estudiante";
+alert(saludo);
+
+let nombre = prompt("Por favor ingresa tu nombre");
+let curso = "JavaScript";
+let email = prompt(
+  `Hola ${nombre}, escribe tu email para recibir mas informacion sobre el curso de ${curso}`
+);
+
+let salario = prompt(`Escribe en pesos tu salario mensual`);
+let promocion = alert(
+  `Al tomar este curso de ${curso} con Coder House, duplicaras tu salario a`
+);
+
+let salarioDoble = salario * 2;
+alert(salarioDoble);
+
+let despedida = alert(
+  `Gracias ${nombre}, nos pondremos en contacto muy pronto sobre el curso de ${curso}`
+);
