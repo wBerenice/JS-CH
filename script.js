c6a0781fa2173b59e456c5d1bf6c8489c3a2c79c
Berenice Sanchez <sanchezberenice09@gmail.com>
2021-12-31 00:18:47 -0600

    Tercer commit

--- script.js
+++ script.js
@@ -23,5 +23,72 @@ let despedida = alert(
 
 //Clase 02
 
-let saludo = "Buen dia estudiante";
-alert(saludo);
+let pregunta1 = prompt("¿Qué signo zodiacal eres?");
+let signo1 = "Aries".toLowerCase();
+let signo2 = "Tauro".toLowerCase();
+let signo3 = "Geminis".toLowerCase();
+let signo4 = "Cancer".toLowerCase();
+let signo5 = "Leo".toLowerCase();
+let signo6 = "Virgo".toLowerCase();
+let signo7 = "Libra".toLowerCase();
+let signo8 = "Escorpion".toLowerCase();
+let signo9 = "Sagitario".toLowerCase();
+let signo10 = "Capricornio".toLowerCase();
+let signo11 = "Acuario".toLowerCase();
+let signo12 = "Piscis".toLowerCase();
+
+if (pregunta1 == signo1) {
+  alert("Eres muy adaptable y puedes llevarte bien con los demas");
+}
+
+if (pregunta1 == signo2) {
+  alert("Eres fuerte y resistente a las adversidades");
+}
+
+if (pregunta1 == signo3) {
+  alert("Eres una persona equilibrada");
+}
+
+if (pregunta1 == signo4) {
+  alert("Eres muy simpático y hogareño");
+}
+
+if (pregunta1 == signo5) {
+  alert("Eres un líder innato");
+}
+
+if (pregunta1 == signo6) {
+  alert("Tienes claro tus planes e ideas para el futuro");
+}
+
+if (pregunta1 == signo7) {
+  alert("Eres una persona muy estable");
+}
+
+if (pregunta1 == signo8) {
+  alert("Tienes habilidades para negociar y alcanzar tus metas");
+}
+
+if (pregunta1 == signo9) {
+  alert("Puedes ser un poco desordenado");
+}
+
+if (pregunta1 == signo10) {
+  alert("Siempre encuentras una solución a las cosas");
+}
+
+if (pregunta1 == signo11) {
+  alert("Das sin esperara nada a cambio");
+}
+
+if (pregunta1 == signo12) {
+  alert("Eres de un carácter tranquilo y alivianado");
+} else {
+  alert(
+    "El dato no es del todo correcto. Refresca esta página para intentarlo de nuevo"
+  );
+}
+
+let aviso = alert(
+  "Gracias por responder (el horóscopo nunca a sido cierto ni lo será. Buenas noches)"
+);
