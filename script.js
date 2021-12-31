809c747126e21aff4bc0df2b7734d7e3fc8ca26f
Berenice Sanchez <sanchezberenice09@gmail.com>
2021-12-31 00:40:22 -0600

    Cuarto commit

--- script.js
+++ script.js
@@ -23,65 +23,47 @@ let despedida = alert(
 
 //Clase 02
 
+let saludo =
+  "Hola querido lector: consulta lo que el universo tiene para ti. No olvides que tus respuestas deberán ser escritas con la primera letra mayúscula";
+alert(saludo);
+
 let pregunta1 = prompt("¿Qué signo zodiacal eres?");
-let signo1 = "Aries".toLowerCase();
-let signo2 = "Tauro".toLowerCase();
-let signo3 = "Geminis".toLowerCase();
-let signo4 = "Cancer".toLowerCase();
-let signo5 = "Leo".toLowerCase();
-let signo6 = "Virgo".toLowerCase();
-let signo7 = "Libra".toLowerCase();
-let signo8 = "Escorpion".toLowerCase();
-let signo9 = "Sagitario".toLowerCase();
-let signo10 = "Capricornio".toLowerCase();
-let signo11 = "Acuario".toLowerCase();
-let signo12 = "Piscis".toLowerCase();
+let signo1 = "Aries";
+let signo2 = "Tauro";
+let signo3 = "Geminis";
+let signo4 = "Cancer";
+let signo5 = "Leo";
+let signo6 = "Virgo";
+let signo7 = "Libra";
+let signo8 = "Escorpion";
+let signo9 = "Sagitario";
+let signo10 = "Capricornio";
+let signo11 = "Acuario";
+let signo12 = "Piscis";
 
 if (pregunta1 == signo1) {
   alert("Eres muy adaptable y puedes llevarte bien con los demas");
-}
-
-if (pregunta1 == signo2) {
+} else if (pregunta1 == signo2) {
   alert("Eres fuerte y resistente a las adversidades");
-}
-
-if (pregunta1 == signo3) {
+} else if (pregunta1 == signo3) {
   alert("Eres una persona equilibrada");
-}
-
-if (pregunta1 == signo4) {
+} else if (pregunta1 == signo4) {
   alert("Eres muy simpático y hogareño");
-}
-
-if (pregunta1 == signo5) {
+} else if (pregunta1 == signo5) {
   alert("Eres un líder innato");
-}
-
-if (pregunta1 == signo6) {
+} else if (pregunta1 == signo6) {
   alert("Tienes claro tus planes e ideas para el futuro");
-}
-
-if (pregunta1 == signo7) {
+} else if (pregunta1 == signo7) {
   alert("Eres una persona muy estable");
-}
-
-if (pregunta1 == signo8) {
+} else if (pregunta1 == signo8) {
   alert("Tienes habilidades para negociar y alcanzar tus metas");
-}
-
-if (pregunta1 == signo9) {
+} else if (pregunta1 == signo9) {
   alert("Puedes ser un poco desordenado");
-}
-
-if (pregunta1 == signo10) {
+} else if (pregunta1 == signo10) {
   alert("Siempre encuentras una solución a las cosas");
-}
-
-if (pregunta1 == signo11) {
+} else if (pregunta1 == signo11) {
   alert("Das sin esperara nada a cambio");
-}
-
-if (pregunta1 == signo12) {
+} else if (pregunta1 == signo12) {
   alert("Eres de un carácter tranquilo y alivianado");
 } else {
   alert(
